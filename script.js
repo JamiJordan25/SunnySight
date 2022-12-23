5f7af97a09e1b6ce8ef6dbece12e79222e20c0b2
Jami Jordan <33480672+JamiJordan25@users.noreply.github.com>
2022-12-23 04:52:41 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 // script.js
 // Get all the images
 image_array = [
-  'Hawa Mahal - The Palace of Winds .jpg',
   'hmedabad, Gujarat, India.jpg',
-  'Jaipur, Rajasthan, India.jpg',
   'Jaipur, Rajasthan, India_4.jpg',
   'Pune, Maharashtra, India.jpg',
   'Pushkar, Rajasthan, India.jpg',
   'Bannerghatta Biological Park, Bengaluru, India.jpg',
   'Kochi, Kerala, India.jpg',
+  'Hawa Mahal - The Palace of Winds2.jpg',
   'Thrissur, India.jpg',
   'Varkala, Kerala, India.jpg',
   'Munroe Island, Kerala, India.jpg',
@@ -26,7 +25,23 @@ image_array = [
   '65, Mall Rd, Mallital, Nainital, Uttarakhand 263002, India.jpg',
   'Mall Road, Nainital, India_1.jpg',
   'Vegas Mall, New Delhi, India.jpg',
-
+  'Ranthambore National Park, Rajasthan, India1.jpg',
+  'Jaipur, Rajasthan, India-1.jpg',
+  'Taj Mahal, Agra, India.jpg',
+  'Querim Beach, Goa, India.jpg',
+  'Amer, Jaipur, India-3.jpg',
+  'Titos Lane 2, Baga, Goa 403516, India.jpg',
+  'Quepem, Goa, India.jpg',
+  'hot_stuff.jpg',
+  'Hot_stuff_2.jpg',
+  'Goa, India.jpg',
+  'Nainital Lake, Ayarpatta, Nainital, Uttarakhand.jpg',
+  'Nainital, Nainital, India.jpg',
+  'Rann of Kutch, India.jpg',
+  'Nainital, Uttarakhand, India3.jpg',
+  'Hawa Mahal, Jaipur, India_2.jpg',
+  'Andheri East, Mumbai, India.jpg',
+  'Enjoy!.jpg',
 ]
 
 function get_random_image(){
